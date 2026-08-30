5a9ab38fa295f23b7776c31ddb5c0ce25bbc8ae3
Flo <you@example.com>
2026-08-30 19:15:39 +0200

    join message

---
+++ index.js
@@ -0,0 +1,29 @@
+const { Client, GatewayIntentBits, EmbedBuilder } = require("discord.js");
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMembers,
+  ],
+});
+
+client.once("ready", () => {
+  console.log(`Bot ist online als ${client.user.tag}`);
+});
+
+client.on("guildMemberAdd", (member) => {
+  const channel = member.guild.systemChannel;
+
+  if (!channel) return;
+
+  const embed = new EmbedBuilder()
+    .setTitle("Willkommen!")
+    .setDescription(`Hey ${member}, willkommen auf **${member.guild.name}**! 🎉`)
+    .setThumbnail(member.user.displayAvatarURL())
+    .setColor(0x6d4aff)
+    .setTimestamp();
+
+  channel.send({ embeds: [embed] });
+});
+
+client.login("DEIN_TOKEN_HIER");
